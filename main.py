14d9667e470878ba2bdc5711981a420278c0ae00
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-02-05 14:15:31 +0000

    update networking

--- main.py
+++ main.py
@@ -15,7 +15,10 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
